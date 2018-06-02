30affe90f2b0a760775c454eba74fe9d5055fd70
AutoMate ToolsAccount <radu.swider@honeywell.com>
2018-06-02 16:41:09 +0530

    update project funds and user funds

--- API/micro funding/src/main/java/com/teamb9/repository/UserFundProjectRepository.java
+++ API/micro funding/src/main/java/com/teamb9/repository/UserFundProjectRepository.java
@@ -1,10 +1,31 @@
 package com.teamb9.repository;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.SqlResultSetMapping;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.teamb9.dto.ProjectDTO;
 import com.teamb9.dto.UserFundProjectDTO;
 
 public interface UserFundProjectRepository extends JpaRepository<UserFundProjectDTO, Long>  {
-
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query(value="UPDATE project_details SET funding_done = (Select SUM(funds) from user_fund_project where project_id = :projectId) "
+			+ "WHERE project_id = :projectId",
+	nativeQuery= true)
+	void updateProjectFunds(@Param("projectId")String projectId);
+	
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query(value="UPDATE users_details SET funding_done = (Select SUM(funds) from user_fund_project where user_id = :userId) "
+			+ "WHERE email = :userId",
+	nativeQuery= true)
+	void updateUserFunds(@Param("userId")String userId);
 }
--- API/micro funding/src/main/java/com/teamb9/service/UserFundProjectService.java
+++ API/micro funding/src/main/java/com/teamb9/service/UserFundProjectService.java
@@ -1,8 +1,18 @@
 package com.teamb9.service;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.PersistenceContext;
+import javax.persistence.SqlResultSetMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.teamb9.controller.UserController;
 import com.teamb9.dto.UserFundProjectDTO;
 import com.teamb9.exception.CustomBadRequestException;
 import com.teamb9.repository.UserFundProjectRepository;
@@ -13,12 +23,18 @@ public class UserFundProjectService {
 	@Autowired
 	UserFundProjectRepository userFundProjectRepository;
 	
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserController.class);
+	
 	public void fundProject(UserFundProjectDTO userFundProjectDTO) 
 			throws CustomBadRequestException {
 		try {
 		userFundProjectRepository.save(userFundProjectDTO);
+		userFundProjectRepository.updateProjectFunds(userFundProjectDTO.getProjectId());
+		userFundProjectRepository.updateUserFunds(userFundProjectDTO.getUserId());
 		}
 		catch(Exception e) {
+			logger.info(e.getMessage());
 			throw new CustomBadRequestException("Bad request");
 		}
 	}
